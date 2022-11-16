e504d083625493c299493c8c47e27910911c0216
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2022-11-16 23:21:15 +0000

    Add courses

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Deployed on Heroku
+[![Heroku](http://heroku-badge.herokuapp.com/?app=rails-corsego&style=flat&svg=1)]
 
+# Deployed on Heroku
 https://rails-corsego.herokuapp.com/
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#landing_page'
+  root 'home#index'
   get 'static_pages/privacy_policy'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
+  resources :courses
 end
