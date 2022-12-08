7944b7157472e13c2fdc90fa76d2d4fd43ce3775
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2022-12-08 00:49:09 +0000

    Add simple form gem and add more fields to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@ User.create!(email: 'maria_adriana@gmail.com', password: 'wazzzup', password_con
     Course.create!([{
       title: Faker::Educator.course_name,
       description: Faker::TvShows::GameOfThrones.quote,
+      short_description: Faker::Quote.famous_last_words,
+      language: Faker::ProgrammingLanguage.name,
+      level: 'Beginner',
+      price: Faker::Number.between(from: 10, to:200),
       user_id: User.first.id
     }])
 end
