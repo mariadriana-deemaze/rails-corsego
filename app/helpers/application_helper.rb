60c31d8c22d532c8aa92280db410f64a59001092
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2023-01-13 15:06:22 +0000

    Add page title helper and dynamic page title implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
-
+    def page_full_title(page_title = '')
+        base_title = "Corsego"
+        if page_title.empty?
+            base_title
+        else
+            base_title + " | " + page_title
+        end
+    end
     # Include it in the helpers (e.g. application_helper.rb)
     include Pagy::Frontend
 end
