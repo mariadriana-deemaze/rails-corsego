de4c23811bf3479362964d07b787e735b46752f8
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2023-01-18 17:47:58 +0000

    Implemente user course progress into helper

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -4,7 +4,9 @@ module CoursesHelper
             if course.user == current_user    
                 link_to "Check analytics", course_path(course), class: "link"
             elsif course.enrollments.where(user: current_user).any?
-                link_to "Go to course", course_path(course), class: "link"
+                link_to course_path(course), class: "link" do
+                    "Progress #{number_to_percentage(course.progress(current_user), precision: 0)}"
+                end
             elsif course.price > 0
                 link_to "Enroll", new_course_enrollment_path(course), class: "primary-button"
             else
