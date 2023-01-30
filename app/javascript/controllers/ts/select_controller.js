2d2bb8f263f7156c2f904b74e3236b3e982adc2a
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2023-01-30 16:44:51 +0000

    TomSelect finally working

--- app/javascript/controllers/ts/select_controller.js
+++ app/javascript/controllers/ts/select_controller.js
@@ -1,9 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
-//import * as TomSelect from "tom-select"
+import TomSelect from "tom-select"
 export default class extends Controller {
   connect() {
     console.log("Hello, Stimulus!", this.element)
-    // new TomSelect(this.element)
+    new TomSelect(this.element)
   }
 }
